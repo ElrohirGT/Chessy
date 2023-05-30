302d6f0d8a0adba50dd2d4cfaec16c5357ed79d9
ElrohirGT <elrohirgt@gmail.com>
2023-05-30 13:26:52 -0600

    feat: Added basic dummy routing for  /game/new.
    This route is the one responsible for creating a new game and creating the WebSocket server.

--- chessy_backend/src/routes/mod.rs
+++ chessy_backend/src/routes/mod.rs
@@ -5,4 +5,5 @@ pub use self::rest_api::*;
 
 pub fn configure_routes(cfg: &mut web::ServiceConfig) {
     cfg.service(web::scope("/user").route("/register", web::post().to(rest_api::register_user)));
+    cfg.service(web::scope("/game").route("/new", web::post().to(rest_api::new_game)));
 }
--- chessy_backend/src/routes/rest_api.rs
+++ chessy_backend/src/routes/rest_api.rs
@@ -1,17 +1,37 @@
 use actix_web::{web, HttpResponse, Responder};
+use serde::Serialize;
 use uuid::Uuid;
 
 use crate::AppState;
 
 pub async fn register_user(name: String, data: web::Data<AppState>) -> impl Responder {
-    let mut users = data.users.lock().await;
     log::debug!("Received username {}", &name);
-
     let id = Uuid::new_v4();
     log::debug!("Generated UUID ({}) for user", &id);
 
+    let mut users = data.users.lock().await;
     users.insert(id, name);
     log::debug!("The current hashmap of users is {:?}", users);
 
     HttpResponse::Ok().message_body(id.hyphenated().to_string())
+}
+
+#[derive(Serialize)]
+pub struct NewGameResponse {
+    #[serde(alias = "socketUrl")]
+    socket_url: String,
+    #[serde(alias = "gameId")]
+    game_id: String,
+}
+
+pub async fn new_game(userId: String, data: web::Data<AppState>) -> impl Responder {
+    log::debug!("User ({}) want's to create a new game!", &userId);
+
+    let socket_url = String::new();
+    let game_id = String::new();
+
+    HttpResponse::Ok().json(NewGameResponse {
+        socket_url,
+        game_id,
+    })
 }
