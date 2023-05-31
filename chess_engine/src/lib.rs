1fed018a43c93cc7c3ebb9d5b8db4a33da42b9db
ElrohirGT <elrohirgt@gmail.com>
2023-05-30 18:29:20 -0600

    feat: Added 2 methods for generating the starting conditions.

--- chess_engine/src/lib.rs
+++ chess_engine/src/lib.rs
@@ -10,3 +10,67 @@ pub struct Game {
     white_king: BoardPosition,
     black_king: BoardPosition,
 }
+
+pub struct GameConfig {
+    white_player_name: String,
+    black_player_name: String,
+    ms_per_player: u64,
+}
+
+pub fn generate_starting_board() -> Board {
+    let cells = (0..8)
+        .map(|i| match i {
+            0 | 1 => (0..8).map(|_| ChessCell::WhitePiece).collect(),
+            6 | 7 => (0..8).map(|_| ChessCell::BlackPiece).collect(),
+            _ => (0..8).map(|_| ChessCell::Empty).collect(),
+        })
+        .collect();
+    Board::new(cells)
+}
+
+pub fn generate_starting_pieces() -> Vec<ChessPiece> {
+    let pawns = vec![PieceColors::White, PieceColors::Black]
+        .into_iter()
+        .flat_map(|color| {
+            let rank = match color {
+                PieceColors::White => "2",
+                PieceColors::Black => "7",
+            };
+            ('a'..='h')
+                .map(|i| {
+                    let pos: &str = &format!("{}{}", i, rank);
+                    let position = pos.try_into().unwrap();
+                    ChessPiece::new(PieceTypes::Pawn, position, color.clone())
+                })
+                .collect::<Vec<ChessPiece>>()
+        })
+        .collect();
+    //TODO Finish implementing
+    pawns
+}
+
+impl Game {
+    pub fn new(
+        GameConfig {
+            white_player_name,
+            black_player_name,
+            ms_per_player,
+        }: GameConfig,
+    ) -> Self {
+        let white_player = Player::new(white_player_name, PieceColors::White, ms_per_player);
+        let black_player = Player::new(black_player_name, PieceColors::Black, ms_per_player);
+        let white_king = BoardPosition::try_from("e1").unwrap();
+        let black_king = BoardPosition::try_from("e8").unwrap();
+        let board = generate_starting_board();
+        let pieces = generate_starting_pieces();
+
+        Game {
+            white_player,
+            black_player,
+            pieces,
+            board,
+            white_king,
+            black_king,
+        }
+    }
+}
