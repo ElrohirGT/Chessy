a6476949205415e39d2fa94d63ad1943d5cd4fcb
ElrohirGT <elrohirgt@gmail.com>
2023-05-30 18:28:53 -0600

    fix: Improved modeling of data and errors

--- chess_engine/src/model.rs
+++ chess_engine/src/model.rs
@@ -1,4 +1,6 @@
-#[derive(Debug)]
+use thiserror::Error;
+
+#[derive(Debug, Clone)]
 pub enum PieceColors {
     Black,
     White,
@@ -20,11 +22,55 @@ pub struct BoardPosition {
     column: ChessFile,
 }
 
+#[derive(Debug, Error)]
+pub enum BoardPositionInstancingErrors {
+    #[error("The format of the file (column) is invalid.")]
+    InvalidColumnFormat(FileInstancingErrors),
+    #[error("The format of the rank (row) is invalid.")]
+    InvalidRowFormat(RankInstancingErrors),
+    #[error("The rank (`{0}`) must be a number.")]
+    RankMustBeANumber(String),
+    #[error("The value (`{0}`) must be of length 2.")]
+    ValueTooLarge(String),
+}
+
+impl TryFrom<&str> for BoardPosition {
+    type Error = BoardPositionInstancingErrors;
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        if value.chars().count() == 2 {
+            let column = ChessFile::try_from(value.chars().nth(0).unwrap())
+                .map_err(|inner| BoardPositionInstancingErrors::InvalidColumnFormat(inner))?;
+            let row: usize = value.get(1..=1).unwrap().parse().map_err(|_| {
+                BoardPositionInstancingErrors::RankMustBeANumber(
+                    value.get(1..=1).unwrap().to_string(),
+                )
+            })?;
+            let row = ChessRank::try_from(row)
+                .map_err(|inner| BoardPositionInstancingErrors::InvalidRowFormat(inner))?;
+            Ok(BoardPosition { column, row })
+        } else {
+            Err(BoardPositionInstancingErrors::ValueTooLarge(
+                value.to_string(),
+            ))
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct ChessPiece {
     kind: PieceTypes,
     position: BoardPosition,
     owner: PieceColors,
+}
+
+impl ChessPiece {
+    pub fn new(kind: PieceTypes, position: BoardPosition, owner: PieceColors) -> ChessPiece {
+        ChessPiece {
+            kind,
+            position,
+            owner,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -34,9 +80,25 @@ pub struct Player {
     ms_remaining: u64,
 }
 
+impl Player {
+    pub fn new(name: String, color: PieceColors, ms_remaining: u64) -> Self {
+        Player {
+            name,
+            color,
+            ms_remaining,
+        }
+    }
+}
+
 #[derive(Debug)]
 pub struct Board {
     cells: Vec<Vec<ChessCell>>,
+}
+
+impl Board {
+    pub fn new(cells: Vec<Vec<ChessCell>>) -> Board {
+        Board { cells }
+    }
 }
 
 #[derive(Debug)]
@@ -55,14 +117,18 @@ pub trait ArrayIndex {
     fn to_index(&self) -> usize;
 }
 
+#[derive(Debug, Error)]
 pub enum FileInstancingErrors {
-    InvalidColumn,
+    #[error("The column must be a char between 'a' and 'h'!")]
+    InvalidColumnChar,
 }
-impl ChessFile {
-    pub fn new(column: char) -> Result<Self, FileInstancingErrors> {
+
+impl TryFrom<char> for ChessFile {
+    type Error = FileInstancingErrors;
+    fn try_from(column: char) -> Result<Self, Self::Error> {
         match column {
             'a'..='h' => Ok(ChessFile(column)),
-            _ => Err(FileInstancingErrors::InvalidColumn),
+            _ => Err(FileInstancingErrors::InvalidColumnChar),
         }
     }
 }
@@ -78,14 +144,18 @@ impl ArrayIndex for ChessFile {
     }
 }
 
+#[derive(Debug, Error)]
 pub enum RankInstancingErrors {
-    InvalidRow,
+    #[error("The row must be a number between 1 and 8!")]
+    InvalidRowChar,
 }
-impl ChessRank {
-    pub fn new(row: usize) -> Result<Self, RankInstancingErrors> {
+
+impl TryFrom<usize> for ChessRank {
+    type Error = RankInstancingErrors;
+    fn try_from(row: usize) -> Result<Self, Self::Error> {
         match row {
             1..=8 => Ok(ChessRank(row)),
-            _ => Err(RankInstancingErrors::InvalidRow),
+            _ => Err(RankInstancingErrors::InvalidRowChar),
         }
     }
 }
