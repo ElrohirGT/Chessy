b0530d9a7f4e8d6476be89dc5b4bf81860c075e9
ElrohirGT <elrohirgt@gmail.com>
2023-06-05 18:43:29 -0600

    feat: Stated the move_piece method.

--- chess_engine/src/lib.rs
+++ chess_engine/src/lib.rs
@@ -1,11 +1,13 @@
 mod game_model;
 mod get_starting_board;
 mod model;
+mod move_piece;
 mod valid_movements_positions;
 
 pub use self::game_model::*;
 pub use self::get_starting_board::*;
 pub use self::model::*;
+pub use self::move_piece::*;
 pub use self::valid_movements_positions::*;
 
 pub fn create_game(config: GameConfig) -> Game {
--- chess_engine/src/main.rs
+++ chess_engine/src/main.rs
@@ -1,8 +1,18 @@
-use chess_engine::get_starting_board;
+use chess_engine::{get_starting_board, move_piece, ChessPiece, PieceColors, PieceTypes};
 use serde_json;
 
 fn main() {
     let board = get_starting_board();
 
-    println!("{}", serde_json::to_string(&board).unwrap());
+    let respone = move_piece(
+        ChessPiece::new(
+            PieceTypes::Pawn,
+            "e1".try_into().unwrap(),
+            PieceColors::White,
+        ),
+        "e2".try_into().unwrap(),
+        board,
+    );
+
+    println!("{}", serde_json::to_string(&respone.unwrap()).unwrap());
 }
