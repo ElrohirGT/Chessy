c4c04fa30fc41a3e31ac8028d1c09956f33b5bcb
ElrohirGT <elrohirgt@gmail.com>
2023-06-12 11:39:51 -0600

    fix: Error returned when the username is empty

--- chessy_backend/src/routes/rest_api.rs
+++ chessy_backend/src/routes/rest_api.rs
@@ -1,19 +1,52 @@
-use actix_web::{web, HttpResponse, Responder};
+use std::fmt::Display;
+
+use actix_web::{web, HttpResponse, Responder, ResponseError, http::header::ContentType};
 use serde::Serialize;
 use uuid::Uuid;
 
 use crate::AppState;
 
-pub async fn register_user(name: String, data: web::Data<AppState>) -> impl Responder {
-    log::debug!("Received username {}", &name);
+#[derive(Debug)]
+pub enum RegisterErrors {
+    NameIsEmpty
+}
+
+impl Display for RegisterErrors {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            RegisterErrors::NameIsEmpty => f.write_str("The user name is empty!"),
+        }
+    }
+}
+
+impl ResponseError for RegisterErrors {
+    fn status_code(&self) -> actix_http::StatusCode {
+        actix_http::StatusCode::BAD_REQUEST
+    }
+
+    fn error_response(&self) -> HttpResponse<actix_http::body::BoxBody> {
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::plaintext())
+            .body(self.to_string())
+    }
+
+}
+
+pub async fn register_user(name: String, data: web::Data<AppState>) -> Result<String, RegisterErrors> {
+    log::debug!("Received username `{}`", &name);
+
+    if name.is_empty() {
+        return Err(RegisterErrors::NameIsEmpty);
+    }
+
     let id = Uuid::new_v4();
-    log::debug!("Generated UUID ({}) for user", &id);
+    log::debug!("Generated UUID `{}` for user `{}`", &id, &name);
 
     let mut users = data.users.lock().await;
     users.insert(id, name);
     log::debug!("The current hashmap of users is {:?}", users);
 
-    HttpResponse::Ok().message_body(id.hyphenated().to_string())
+    Ok(id.hyphenated().to_string())
 }
 
 #[derive(Serialize)]
