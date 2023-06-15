d3ba899d935ec4b079444684a330f073f00cbae0
Daniel Rayo <ray22933@uvg.edu.gt>
2023-06-14 19:21:51 -0600

    Add : URL constant for API request

--- chessy_frontend/src/main.js
+++ chessy_frontend/src/main.js
@@ -6,6 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// API REQUEST URL
+export const REGISTER_URL = "http://127.0.0.1:8080/user/register"
+export const GAME_URL = "http://127.0.0.1:8080/user/register"
+
 const app = createApp(App)
 
 app.use(createPinia())
