e0175ac638676308ee14a8f43517ea4821b6bac9
Daniel Rayo <ray22933@uvg.edu.gt>
2023-06-22 12:13:18 -0600

    Feat: Create new buttonImportant component.

--- chessy_frontend/src/main.js
+++ chessy_frontend/src/main.js
@@ -1,4 +1,3 @@
-import '@/assets/reset.css'
 import '@/assets/base.css'
 
 import { createApp } from 'vue'
@@ -8,8 +7,8 @@ import App from './App.vue'
 import router from './router'
 
 // API REQUEST URL
-export const REGISTER_URL = "http://127.0.0.1:8080/user/register"
-export const GAME_URL = "http://127.0.0.1:8080/user/register"
+export const REGISTER_URL = 'http://127.0.0.1:8080/user/register'
+export const GAME_URL = 'http://127.0.0.1:8080/user/register'
 
 const app = createApp(App)
 
