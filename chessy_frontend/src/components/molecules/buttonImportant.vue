33dea6d3dc53435c0edb078364e85291439d2815
Daniel Rayo <ray22933@uvg.edu.gt>
2023-06-24 23:32:19 -0600

    Feat: Finish v1 of LOGIN page.

--- chessy_frontend/src/components/molecules/buttonImportant.vue
+++ chessy_frontend/src/components/molecules/buttonImportant.vue
@@ -1,18 +1,18 @@
 <template>
-  <button class="pushable">
-    <span class="shadow"></span>
-    <span class="edge"></span>
-    <span class="front">
-      <slot></slot>
-    </span>
-  </button>
+    <button class="pushable">
+        <span class="shadow"></span>
+        <span class="edge"></span>
+        <span class="front">
+            <slot></slot>
+        </span>
+    </button>
 </template>
 
 <script>
 export default {
-  setup() {
-    return {}
-  }
+    setup() {
+        return {}
+    }
 }
 </script>
 
@@ -20,64 +20,64 @@ export default {
 @import '../../assets/colors';
 
 .front {
-  display: block;
-  padding: 1ch 2ch;
-  border-radius: 12px;
-  background-color: var(--primary-button);
-  color: var(--text);
-  transform: translateY(-4px);
-  font-size: 1rem;
-  font-weight: bold;
-  will-change: transform;
-  transition: transform 250ms;
+    display: block;
+    padding: 1ch 2ch;
+    border-radius: 12px;
+    background-color: var(--primary-button);
+    color: var(--text);
+    transform: translateY(-4px);
+    font-size: 1rem;
+    font-weight: bold;
+    will-change: transform;
+    transition: transform 250ms;
 }
 .pushable {
-  position: relative;
-  background: transparent;
-  border: none;
-  padding: 0;
-  cursor: pointer;
-  outline-offset: 5px;
-  -webkit-tap-highlight-color: transparent;
+    position: relative;
+    background: transparent;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+    outline-offset: 5px;
+    -webkit-tap-highlight-color: transparent;
 }
 
 .shadow {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 12px;
-  background-color: hsl(0deg 0% 0% / 0.25);
-  transform: translateY(2px);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 12px;
+    background-color: hsl(0deg 0% 0% / 0.25);
+    transform: translateY(2px);
 }
 .edge {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  border-radius: 12px;
-  background: var(--primary-button-dark);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 12px;
+    background: var(--primary-button-dark);
 }
 
 .pushable:focus:not(:focus-visible) {
-  outline: none;
+    outline: none;
 }
 .pushable:hover .front {
-  transform: translateY(-6px);
-  transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
+    transform: translateY(-6px);
+    transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
 }
 .pushable:active .front {
-  transform: translateY(-2px);
-  transition: transform 34ms;
+    transform: translateY(-2px);
+    transition: transform 34ms;
 }
 .pushable:hover .shadow {
-  transform: translateY(4px);
-  transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
+    transform: translateY(4px);
+    transition: transform 250ms cubic-bezier(0.3, 0.7, 0.4, 1.5);
 }
 .pushable:active .shadow {
-  transform: translateY(1px);
-  transition: transform 34ms;
+    transform: translateY(1px);
+    transition: transform 34ms;
 }
 </style>
--- chessy_frontend/src/views/LoginView.vue
+++ chessy_frontend/src/views/LoginView.vue
@@ -1,38 +1,92 @@
 <template>
-  <main>
-    <h1>Login View</h1>
-    <textForm :fields="this.fields" title="Login" @submit-form="registerUser"></textForm>
-  </main>
+    <main>
+        <div class="app-title">
+            <i class="fa-regular fa-chess-knight"></i>
+            <h1>Chessy</h1>
+        </div>
+        <form-card
+            title="Login"
+            :fields="this.fields"
+            submitButtonText="Start Playing "
+            submitButtonIcon="fa-solid fa-arrow-right"
+            @form-submitted="fetchFieldsData()"
+        ></form-card>
+    </main>
 </template>
 
 <script>
-import formField from '../components/formField.js'
-import textForm from '../components/textForm.vue'
-import {REGISTER_URL} from '../main.js'
+import formCard from '@/components/organisms/formCard.vue'
+import { REGISTER_URL } from '../main.js'
 
 export default {
-  components: {
-    textForm
-  },
-  data() {
-    return {
-      fields: [new formField('username')]
-    }
-  },
-  methods: {
-    async registerUser() {
-        let options = {
-              method: "POST",
-              headers: { "Content-Type": "text/plain" },
-              body: this.fields[0].currentValue,
-            };
+    components: {
+        formCard
+    },
+    data() {
+        return {
+            fields: [
+                {
+                    id: 'username',
+                    inputType: 'text',
+                    placeholder: 'SuperChessGamer, jajaja',
+                    currentValue: null
+                },
+                {
+                    id: 'password',
+                    inputType: 'password',
+                    placeholder: 'Super secret he he he...',
+                    currentValue: null
+                }
+            ]
+        }
+    },
+    methods: {
+        fetchFieldsData() {
+            console.log('fetching datac')
+            console.log(this.fields)
+        },
+        async registerUser() {
+            let options = {
+                method: 'POST',
+                headers: { 'Content-Type': 'text/plain' },
+                body: this.fields[0].currentValue
+            }
 
-        fetch( REGISTER_URL, options)
-        .then(response=>(response.text()))
-        .then(text=>(console.log(text)))
+            fetch(REGISTER_URL, options)
+                .then((response) => response.text())
+                .then((text) => console.log(text))
+        }
     }
-  }
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+@import '../assets/colors.css';
+main {
+    width: 100vw;
+    height: 100vh;
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 3ch;
+}
+
+.app-title {
+    display: flex;
+    align-items: center;
+    margin-bottom: 3ch;
+}
+.app-title > img {
+    height: 3rem;
+}
+.app-title > h1,
+.app-title > i {
+    font-size: 3rem;
+    font-weight: bold;
+}
+.app-title > h1 {
+    margin-left: 0.5ch;
+}
+</style>
