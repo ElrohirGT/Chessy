ab39ad646d1478a5a72422c9465cd4e27a28cf3f
Daniel Rayo <ray22933@uvg.edu.gt>
2023-06-30 10:01:36 -0600

    Add: Create buttonHero component

---
+++ chessy_frontend/src/components/molecules/buttonHero.vue
@@ -0,0 +1,16 @@
+<template>
+    <a href="#">
+        <span>Ara ara!</span>
+        <span aria-hidden="true">Ara ara!</span>
+    </a>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
