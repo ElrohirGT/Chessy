03dad6728b08a310e20b2f2031276ad2554322a3
DanielRasho <danirayoroldan@gmail.com>
2023-12-18 10:11:48 -0600

    Fix: Moving Hex color codes to its own object.

---
+++ chessy_frontend/src/components/Color.js
@@ -0,0 +1,8 @@
+export const COLOR = Object.freeze({
+    WHITE_COLOR: '#ffffff',
+    BLACK_COLOR: '#000000',
+    GREY_COLOR: '#b8b8b8',
+    RED_COLOR: '#d92323',
+    PINK_COLOR: '#ff1ead',
+    LIGHT_BLUE_COLOR: '#31e7e4'
+})
