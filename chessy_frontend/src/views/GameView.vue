a6bc20b4677d8000d082b0514476049968339fe6
DanielRasho <danirayoroldan@gmail.com>
2023-12-18 10:12:06 -0600

    Feat: Add Game view assets

--- chessy_frontend/src/views/GameView.vue
+++ chessy_frontend/src/views/GameView.vue
@@ -1,9 +1,28 @@
 <template>
-    <main>
-        <h1>Game View</h1>
+    <main class="root">
+        <div class="left-container"></div>
+        <div class="chess-board"></div>
+        <div class="right-container"></div>
     </main>
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+.root {
+    width: 100vw;
+    max-width: 100vw;
+    height: 100vh;
+    background-image: url('../assets/Game/background.webp');
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.root > * {
+    height: 100px;
+    width: 100px;
+    background-color: red;
+}
+</style>
