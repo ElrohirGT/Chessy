a9fcd59a31f00fdee9511e1c597ca550a1899bac
DanielRasho <ray22933@uvg.edu.gt>
2024-08-19 22:40:24 -0600

    fix: change vite base depending on the env (production, development)

--- chessy_frontend/vite.config.js
+++ chessy_frontend/vite.config.js
@@ -1,14 +1,28 @@
 import { fileURLToPath, URL } from 'node:url'
+import process from 'node:process'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  console.log("================")
+  console.log(mode)
+  console.log("================")
+
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [vue()],
+    base: env.VITE_BASE || '/',
+    server: {
+      host: '127.0.0.1',
+      port: 3000
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
 })
