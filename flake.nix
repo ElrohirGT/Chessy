da8259d87c7b26ad69efcb8ca540418ff1fd0e6c
ElrohirGT <elrohirgt@gmail.com>
2024-08-30 15:04:13 -0600

    feat: Added flake.nix

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Chessy flake for reproducible builds!";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+  }: let
+    forAllSystems = {
+      pkgs ? nixpkgs,
+      function,
+    }:
+      nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-macos"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ]
+      (system:
+        function {
+          pkgs = import pkgs {
+            inherit system;
+            config.allowUnfree = true;
+            overlays = [
+              rust-overlay.overlays.default
+            ];
+          };
+          inherit system;
+        });
+  in {
+    devShells = forAllSystems {
+      function = {pkgs, ...}: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            (rust-bin.fromRustupToolchainFile ./rust-toolchain)
+            openssl
+            pkg-config
+            nodejs
+            yarn
+            wasm-pack
+            websocat
+          ];
+
+          shellHook = ''
+            export RUST_BACKTRACE=1
+          '';
+        };
+      };
+    };
+  };
+}
